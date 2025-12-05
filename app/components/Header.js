04999d67aeeaedb88e0aad3680d1729df63f0763
luwatt6-del <luwatt6@gmail.com>
2025-12-04 20:53:33 -0400

    Create Header.js

---
+++ app/components/Header.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-purple-900 text-gold-400 p-6 mt-16 text-center">
+      <p>© 2025 Grace & Faith Ministries, Bridgetown, Barbados</p>
+      <p>Email: graceandfaithministrys@yahoo.com</p>
+    </footer>
+  );
+}
