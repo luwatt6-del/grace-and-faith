2193e84a5da9bfe57221ed594ab9708a1ed62fb9
luwatt6-del <luwatt6@gmail.com>
2025-12-08 12:47:22 -0400

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Grace & Faith Ministries</h1>
+      <p>Welcome to our official website! We are honored to have you here.</p>
+    </main>
+  );
+}
