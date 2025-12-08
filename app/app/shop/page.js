39169647b957f2b3d4724e9026e997f13d4eed9b
luwatt6-del <luwatt6@gmail.com>
2025-12-08 13:21:26 -0400

    Create page.js

---
+++ app/app/shop/page.js
@@ -0,0 +1,8 @@
+export default function ShopPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Online Shop</h1>
+      <p>The shop will be launched in the near future. Stay connected!</p>
+    </main>
+  );
+}
