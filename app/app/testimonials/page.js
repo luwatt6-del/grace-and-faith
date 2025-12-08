d95c63646240441a2a2de199fd75a0af5de2f651
luwatt6-del <luwatt6@gmail.com>
2025-12-08 13:18:15 -0400

    Create page.js

---
+++ app/app/testimonials/page.js
@@ -0,0 +1,8 @@
+export default function TestimonialsPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Testimonials</h1>
+      <p>Testimonies of God’s goodness will appear here.</p>
+    </main>
+  );
+}
