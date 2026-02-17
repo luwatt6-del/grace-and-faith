10e8219f680e44042ec328befcc4a0f03a9909d6
luwatt6-del <luwatt6@gmail.com>
2026-02-17 14:37:23 -0400

    Update page.js

--- app/newsletter/page.js
+++ app/newsletter/page.js
@@ -1,1 +1,24 @@
+export default function NewsletterPage() {
+  return (
+    <div>
+      <h1>Newsletter</h1>
+      <p>Stay connected with Grace & Faith Ministries.</p>
+
+      <form>
+        <div>
+          <label>Name</label><br />
+          <input type="text" name="name" />
+        </div>
+
+        <div>
+          <label>Email</label><br />
+          <input type="email" name="email" />
+        </div>
+
+        <br />
+        <button type="submit">Subscribe</button>
+      </form>
+    </div>
+  );
+}
 
