ee1ada73c4ef2f749750de1bde7c58e2096e66f7
luwatt6-del <luwatt6@gmail.com>
2026-04-17 19:36:52 -0400

    Create page.js

---
+++ app/devotional/issue5/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Issue5Page() {
+  return (
+    <div style={styles.container}>
+
+      <h1>God’s Timing is Perfect</h1>
+
+      <p style={styles.date}>April 13, 2026</p>
+
+      <p style={styles.scripture}>
+        “He has made everything beautiful in its time…”
+        <br />— Ecclesiastes 3:11
+      </p>
+
+      <p>
+        
